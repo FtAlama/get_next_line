e9ce6f5171bcddee3f3e09900d8e77eb608cd908
Ajit Lama <alama@shi-r3-p4.s19.be>
2024-05-12 22:09:21 +0200

    re do with out linked list

--- get_next_line.c
+++ get_next_line.c
@@ -6,109 +6,59 @@
 /*   By: alama <alama@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 18:52:02 by alama             #+#    #+#             */
-/*   Updated: 2024/05/10 23:20:58 by alama            ###   ########.fr       */
+/*   Updated: 2024/05/12 22:08:47 by alama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-void	ft_add_read_line(t_list **list, int fd)
+char	*ft_add_str_buffer_size(char *str, int fd)
 {
-	char	*str;
-	int	rd;
+	char	*buffer;
+	int		rd;
 
 	rd = 1;
-	while (rd != 0 && ft_find_list_n(list) == 0)
+	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	while (rd > 0 && ft_find_line(str) == 0)
 	{
-		str = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-		if (!str)
-		{
-			ft_free_list(list);
-			return ;
-		}
-		rd = read(fd, str, BUFFER_SIZE);
-		if (!rd)
-		{
-			free(str);
-			ft_free_list(list);
-			return ;
-		}
-		str[rd] = '\0';
-		if (ft_add_str_list(list, str) == 0)
-			return ;
+		rd = read(fd, buffer, BUFFER_SIZE);
+		str = str_join(str, buffer);
 	}
-}
-
-int	ft_count_str_size(t_list	**list)
-{
-	t_list	*tmp;
-	int		i;
-	int		count;
-	
-	tmp = *list;
-	count = 0;
-	while (tmp)
-	{
-		i = 0;
-		while (tmp->content[i])
-		{
-			if (tmp->content[i] == '\n')
-				return (count + 1);
-			i++;
-			count++;
-		}
-		tmp = tmp->next;
-	}
-	return (count);
-}
-
-char	*ft_trim_str(t_list **list)
-{
-	t_list	*tmp;
-	char	*str;
-	int		i;
-	int		size;
-	int		j;
-	
-	if (*list == NULL)
-		return (NULL);
-	tmp = *list;
-	size = ft_count_str_size(list);
-	str = malloc(sizeof(char) * size);
-	j = 0;
-	while (tmp)
-	{
-		i = 0;
-		while (tmp->content[i] && size > j)
-		{
-			str[j] = tmp->content[i];
-			i++;
-			j++;
-		}
-		tmp = tmp->next;
-	}
-	str[j] = '\0';
 	return (str);
+}
+
+char	*str_trim(char *str)
+{
+	char	*buffer;
+	int		i;
+
+	i = 0;
+	while (str[i] != '\n' && str[i]) 
+		i++;
+	buffer = malloc(sizeof(char) * (i + 1));
+	i = 0;
+	while (str[i] && str[i] != '\n')
+	{
+		buffer[i] = str[i];
+		i++;
+	}
+	buffer[i] = '\0';
+	return (buffer);
 }
 
 char	*get_next_line(int fd)
 {
-	static t_list	*list;
-	char			*str;
-	t_list			*tmp;
+	static char	*str;
+	char	*buffer;
 	
-	if (list)
+	if (!str)
 	{
-		tmp = list;
-		ft_next_iterre(&tmp);
+		str = malloc(sizeof(char) * 1);
+		str[0] = '\0';
 	}
-	list = NULL;
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) == -1)
-		return (NULL);
-	ft_add_read_line(&list, fd);
-	if (tmp)
-		ft_add_front(&list, &tmp);
-	str = ft_trim_str(&list);
-	return (str);
+	str = ft_add_str_buffer_size(str, fd);
+	buffer = str_trim(str);
+	str = next_line(str);
+	return (buffer);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,111 +6,84 @@
 /*   By: alama <alama@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 23:40:51 by alama             #+#    #+#             */
-/*   Updated: 2024/05/10 23:20:49 by alama            ###   ########.fr       */
+/*   Updated: 2024/05/12 22:08:43 by alama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-void	ft_free_list(t_list **list)
+int	ft_strlen(char *str)
 {
-	t_list	*tmp;
-
-	while (list)
-	{
-		tmp = *list;
-		free((*list)->content);
-		*list = (*list)->next;
-		free(tmp);
-	}
-}
-
-int	ft_add_str_list(t_list **list, char *str)
-{
-	t_list	*tmp;
-	t_list	*new;
-
-	new = malloc(sizeof(t_list));
-	if (!new)
-	{
-		ft_free_list(list);
-		return (0);
-	}
-	new->content = str;
-	new->next = NULL;
-	tmp = *list;
-	if (*list == NULL)
-		*list = new;
-	else
-	{
-		while (tmp->next)
-			tmp = tmp->next;
-		tmp->next = new;
-	}
-	return (1);
-}
-
-int	ft_find_list_n(t_list **list)
-{
-	t_list	*tmp;
 	int	i;
 
-	if (!list)
-		return (0);
-	tmp = *list;
-	while (tmp)
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*str_join(char *str, char *buffer)
+{
+	char	*join;
+	int	i;
+	int	j;
+	
+	//printf("%d		-		%d\n", ft_strlen(str), ft_strlen(buffer));
+	join = malloc(sizeof(char) * (ft_strlen(str) + ft_strlen(buffer) + 1));
+	j = 0;
+	i = 0;
+	while (str[i])
 	{
-		i = 0;
-		while (tmp->content[i])
-		{
-			if (tmp->content[i] == '\n')
-				return (1);
-			i++;
-		}
-		tmp = tmp->next;
+		join[j] = str[i];
+		i++;
+		j++;	
+	}
+	i = 0;
+	while (buffer[i])
+	{
+		join[j] = buffer[i];
+		i++;
+		j++;
+	}
+	join[j] = '\0';
+	free(str);
+	return (join);
+}
+
+int	ft_find_line(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (i);
+		i++;
 	}
 	return (0);
 }
 
-void	ft_next_iterre(t_list **list)
+char	*next_line(char *str)
 {
-	char	*str;
-	t_list	*tmp;
+	char	*tmp;
 	int		i;
 	int		j;
-
-	tmp = *list;
-	while (tmp->next)
-		tmp = tmp->next;
-	i = 0;
-	while (tmp->content[i] && tmp->content[i] != '\n')
-		i++;
+	
+	i = ft_find_line(str) + 1;
 	j = i;
-	while (tmp->content[i])
+	while (str[i])
 		i++;
-	str = malloc(sizeof(char) * (i - j + 1));
+	tmp = malloc(sizeof(char) * (i - j + 1));
 	i = 0;
-	while (tmp->content[j])
+	while (str[j])
 	{
-		str[i] = tmp->content[j];
+		tmp[i] = str[j];
 		j++;
 		i++;
 	}
-	str[i] = '\0';
-	free(tmp->content);
-	tmp->content = str;	
-}
-
-void	ft_add_front(t_list **list, t_list **new)
-{
-	t_list	*tmp;
-	t_list	*last_new;
-	
-	last_new = *new;
-	while (last_new->next)
-		last_new = last_new->next;
-	tmp = *list;
-	*list = last_new;
-	(*list)->next = tmp;
+	tmp[i] = '\0';
+	free(str);
+	return (tmp);
 }
